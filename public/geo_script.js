e4efce322ff57a4f2febf841dda58e66eb57ce43
Sheonna Harris <svonharris@gmail.com>
2015-02-11 18:32:09 -0500

    Added another form field that allows users to enter a city (with state abrev optional) and return a map image of the city.

--- public/geo_script.js
+++ public/geo_script.js
@@ -26,3 +26,26 @@ $('.get-geo').on('click', function() {
   var src = 'https://maps.googleapis.com/maps/api/staticmap?zoom=7&size=600x300&center=' + latitude + ',' + longitude;
   $('.map').html('<img src="' + src + '"/>');
 });
+
+
+
+//Find given city location on map
+$('.city').on('click', function() {
+  var place = $('input#city').val();
+  console.log(place);
+  
+  $.ajax({
+    type: 'GET',
+    url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22' + place + '%22&format=json&callback=',
+    dataType: 'json'
+  }).done(function(data){
+    console.log(data.query.results.Result.latitude);
+    var lat = data.query.results.Result.latitude;
+    var long = data.query.results.Result.longitude;
+    var src = 'https://maps.googleapis.com/maps/api/staticmap?zoom=13&size=600x300&center=' + lat + ',' + long;
+  $('.map').html('<img src="' + src + '"/>');
+  }).error(function(error) {
+    console.log(error);
+  });
+  
+});
